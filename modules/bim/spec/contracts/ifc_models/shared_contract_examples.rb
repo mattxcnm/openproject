22f365af000f0f00da51d74781a4abeeb7587580
Klaus Zanders <k.zanders@openproject.com>
2023-09-25 15:31:55 +0200

    Remove mocking from shared examples (is done in test)

--- modules/bim/spec/contracts/ifc_models/shared_contract_examples.rb
+++ modules/bim/spec/contracts/ifc_models/shared_contract_examples.rb
@@ -39,12 +39,8 @@ RSpec.shared_examples_for 'ifc model contract' do
   before do
     allow(ifc_model).to receive(:ifc_attachment).and_return(ifc_attachment)
 
-    allow(other_user).to receive(:allowed_to?) do |permission, permission_project|
-      permissions.include?(permission) && model_project == permission_project
-    end
-
-    allow(current_user).to receive(:allowed_to?) do |permission, permission_project|
-      permissions.include?(permission) && model_project == permission_project
+    mock_permissions_for(current_user) do |mock|
+      mock.in_project *permissions, project: permission_project
     end
   end
 
